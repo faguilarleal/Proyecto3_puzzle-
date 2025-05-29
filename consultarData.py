4b63692defa27f4d7cd3867875e07b90a361754c
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2025-05-29 15:12:19 -0600

    feat: add consultar data console mode

---
+++ consultarData.py
@@ -0,0 +1,32 @@
+import requests
+
+def resolver_rompecabezas():
+    print("=== RESOLVER ROMPECABEZAS ===")
+    try:
+        piece_id = int(input("Ingresa el ID de la pieza inicial: "))
+    except ValueError:
+        print("El ID debe ser un numero entero.")
+        return
+
+    url = f"http://localhost:5000/assemble/full/{piece_id}"
+
+    print(f"\nConsultando pasos desde la pieza {piece_id}...\n")
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        pasos = response.json()
+
+        if not pasos:
+            print("No se encontraron pasos, revise las  relaciones desde esta pieza")
+        else:
+            print("=== PASOS PARA RESOLVER ===")
+            for i, paso in enumerate(pasos, start=1):
+                print(f"{i}. {paso}")
+                
+    except requests.exceptions.ConnectionError:
+        print("No se pudo conectar al servidor Flask")
+    except Exception as e:
+        print("Error al consultar los pasos:", e)
+
+if __name__ == "__main__":
+    resolver_rompecabezas()
